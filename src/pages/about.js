87d295dc527fdc723a5ddabae7dc141fddb7f833
Jason Tuyen <jtuyen99@gmail.com>
2021-05-12 00:15:06 -0700

    Added some links to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -15,18 +15,17 @@ export default function About() {
       <p>I'm Jason Tuyen and I often go by the alias of iCookieStalker. If you're reading this, thanks for visiting my website!</p>
       <p>
         I am currently studying Computer Science and Education (specialization in Equity and Engagement) in California.
-        My overall goal is to pursue a career in Software Engineering and to build products that helps others. 
+        My overall goal is to pursue a career in Software Engineering and to build products that will help others. 
         My biggest values are accessibility and simplicity which is why I enjoyed my previous jobs
         as a Special Education Tutor and IT Tech Support.
       </p>
       <p>
-        Outside of technolgy and coding I enjoy playing video games, rocking out on my drums, and binging anime + k-dramas.
-        My other interests include making music playlists, editing videos, and searching for the best cookie. 
-        I've also picked up new hobbies during the COVID-19 pandemic such as longboard dancing and photography.
+        Outside of technolgy and coding I enjoy <a href="https://www.youtube.com/channel/UCUzAAvRtuSh6q2MI3jy52Dw">playing video games</a>, rocking out on my drums, and binging anime + k-dramas.
+        My other interests include videography/photography, <a href="https://open.spotify.com/user/icookiestalker/playlists">making music playlists</a>, searching for the best cookie, and most recently longboard dancing.
       </p>
       <p>
-        My two favorite accomplishments are when I won a rap competition and when I helped edit a short film. But accomplishments are better
-        as a team so if you want to collab feel free to send me a message from any of the links below.
+        My two recent and favorite accomplishments are when <a href="https://twitter.com/iCookieStalker/status/1325620776523821056">I wrote a rap song during my first hackathon</a> and when I helped <a href="https://www.youtube.com/channel/UCU8LhZGkKsmXddsQjARLmsA/videos">edit for my University's Pilipino Cultural Night.</a> 
+        But accomplishments are better as a team so if you want to collab feel free to send me a message from any of the links below.
       </p>
       <p><b>Good Luck Have Fun,<br></br>Jason "iCookieStalker" Tuyen</b></p>
     </Layout>
